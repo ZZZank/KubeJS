c7b0ea7d889fcf68273f5dcd3051b3a1acf07027
ZZZank <3410764033@qq.com>
2024-05-19 21:51:32 +0800

    reloadable native events, wow

--- common/src/main/java/dev/latvian/kubejs/event/PlatformEventHandler.java
+++ common/src/main/java/dev/latvian/kubejs/event/PlatformEventHandler.java
@@ -2,15 +2,18 @@ package dev.latvian.kubejs.event;
 
 import dev.architectury.injectables.annotations.ExpectPlatform;
 import dev.latvian.mods.rhino.util.DynamicFunction;
+import org.jetbrains.annotations.Contract;
 
 public abstract class PlatformEventHandler implements DynamicFunction.Callback {
 
 	@ExpectPlatform
+	@Contract(value = " -> _")
 	public static PlatformEventHandler instance() {
 		throw new AssertionError("Not implemented");
 	}
 
 	@ExpectPlatform
+	@Contract(value = " -> _")
 	public void unregister() {
 		throw new AssertionError("Not implemented");
 	}
