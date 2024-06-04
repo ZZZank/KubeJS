f959ec778e4294bc70a1fc6e1147313b4d99ac99
ZZZank <3410764033@qq.com>
2024-06-04 13:09:12 +0800

    lang event rework

--- common/src/main/java/dev/latvian/kubejs/client/LangEventJS.java
+++ common/src/main/java/dev/latvian/kubejs/client/LangEventJS.java
@@ -1,6 +1,8 @@
 package dev.latvian.kubejs.client;
 
 import com.github.bsideup.jabel.Desugar;
+import com.google.gson.JsonObject;
+import dev.latvian.kubejs.KubeJS;
 import dev.latvian.kubejs.event.EventJS;
 import dev.latvian.kubejs.item.ItemStackJS;
 import net.minecraft.core.Registry;
@@ -8,6 +10,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
 
@@ -18,67 +21,95 @@ public class LangEventJS extends EventJS {
 	public static final Pattern PATTERN = Pattern.compile("[a-z_]+");
 
 	@Desugar
-	public record Key(String namespace, String lang, String key) {
-	}
-
-	public final String lang;
-	public final Map<Key, String> map;
-
-	public LangEventJS(String lang, Map<Key, String> map) {
-		this.lang = lang;
-		this.map = map;
-	}
-
-	public void add(String namespace, String key, String value) {
-		if (namespace == null || key == null || value == null || namespace.isEmpty() || key.isEmpty() || value.isEmpty()) {
-			throw new IllegalArgumentException("Invalid namespace, key or value: [" + namespace + ", " + key + ", " + value + "]");
+	public record LangEntries(String namespace, String lang, Map<String,String> entries) {
+		public LangEntries(String namespace, String lang) {
+			this(namespace, lang, new HashMap<>());
 		}
 
-		map.put(new Key(namespace, lang, key), value);
-	}
+		public void add(String key, String text) {
+			ensureValid(key, text);
+			entries.put(key, text);
+		}
 
-	public void addAll(String namespace, Map<String, String> map) {
-		for (var e : map.entrySet()) {
-			add(namespace, e.getKey(), e.getValue());
+		public void addAll(Map<String, String> map) {
+			for (var entry : map.entrySet()) {
+				add(entry.getKey(), entry.getValue());
+			}
+		}
+
+		public void addIfAbsent(String key, String text) {
+			ensureValid(key, text);
+			entries.putIfAbsent(key, text);
+		}
+
+		public JsonObject asJson() {
+			JsonObject obj = new JsonObject();
+			for (var entry : entries.entrySet()) {
+				obj.addProperty(entry.getKey(), entry.getValue());
+			}
+			return obj;
+		}
+
+		public static void ensureValid(String key, String text) {
+			if (key == null || text == null || key.trim().isEmpty()) {
+				throw new IllegalArgumentException("Invalid key or value: [" + key + ", " + text + "]");
+			}
 		}
 	}
 
-	public void add(String key, String value) {
-		add("minecraft", key, value);
+	public final Map<String, Map<String, LangEntries>> namespace2lang2entries;
+
+	public LangEventJS() {
+		namespace2lang2entries = new HashMap<>();
 	}
 
-	public void addAll(Map<String, String> map) {
-		addAll("minecraft", map);
+	public LangEntries get(String namespace, String lang) {
+		if (namespace == null || lang == null || namespace.isEmpty() || lang.isEmpty()) {
+			throw new IllegalArgumentException("Invalid namespace or lang: [" + namespace + ", " + lang + "]");
+		}
+		var namespaced = namespace2lang2entries.get(namespace);
+		if (namespaced == null) {
+			namespaced = new HashMap<>();
+			namespace2lang2entries.put(namespace, namespaced);
+		}
+		var entries = namespaced.get(lang);
+		if (entries == null) {
+			entries = new LangEntries(namespace, lang);
+			namespaced.put(lang, entries);
+		}
+		return entries;
 	}
 
-	public void renameItem(ItemStackJS stack, String name) {
+	public LangEntries get(String lang) {
+		return get(KubeJS.MOD_ID, lang);
+	}
+
+	public void renameItem(String lang, ItemStackJS stack, String name) {
 		if (stack == null || stack.isEmpty()) {
 			return;
 		}
 		var desc = stack.getItem().getDescriptionId();
-
 		if (desc != null && !desc.isEmpty()) {
-			add(stack.getMod(), desc, name);
+			get(stack.getMod(), lang).add(desc, name);
 		}
 	}
 
-	public void renameBlock(Block block, String name) {
+	public void renameBlock(String lang, Block block, String name) {
 		if (block == null || block == Blocks.AIR) {
 			return;
 		}
-		var d = block.getDescriptionId();
-
-		if (d != null && !d.isEmpty()) {
+		var desc = block.getDescriptionId();
+		if (desc != null && !desc.isEmpty()) {
 			var modid = Registry.BLOCK.getKey(block).getNamespace();
-			add(modid, d, name);
+			get(modid, lang).add(desc, name);
 		}
 	}
 
-	public void renameEntity(ResourceLocation id, String name) {
-		add(id.getNamespace(), "entity." + id.getNamespace() + "." + id.getPath().replace('/', '.'), name);
+	public void renameEntity(String lang, ResourceLocation id, String name) {
+		get(id.getNamespace(), lang).add("entity." + id.getNamespace() + "." + id.getPath().replace('/', '.'), name);
 	}
 
-	public void renameBiome(ResourceLocation id, String name) {
-		add(id.getNamespace(), "biome." + id.getNamespace() + "." + id.getPath().replace('/', '.'), name);
+	public void renameBiome(String lang, ResourceLocation id, String name) {
+		get(id.getNamespace(), lang).add("biome." + id.getNamespace() + "." + id.getPath().replace('/', '.'), name);
 	}
 }
