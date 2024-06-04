b435fb688ace32ff0c18f59113ce7e9b2d817af6
ZZZank <3410764033@qq.com>
2024-06-04 11:10:03 +0800

    make KubeJS textures overridable by user resource packs

--- common/src/main/java/dev/latvian/kubejs/client/KubeJSClientResourcePack.java
+++ common/src/main/java/dev/latvian/kubejs/client/KubeJSClientResourcePack.java
@@ -2,10 +2,12 @@ package dev.latvian.kubejs.client;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import dev.latvian.kubejs.KubeJS;
 import dev.latvian.kubejs.generator.AssetJsonGenerator;
 import dev.latvian.kubejs.script.data.KubeJSResourcePack;
 import dev.latvian.kubejs.util.KubeJSPlugins;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.FilePackResources;
 import net.minecraft.server.packs.PackResources;
 import net.minecraft.server.packs.PackType;
 
@@ -16,8 +18,19 @@ import java.util.Map;
 
 public class KubeJSClientResourcePack extends KubeJSResourcePack {
 	public static List<PackResources> inject(List<PackResources> packs) {
+		if (KubeJS.instance == null) {
+			return packs;
+		}
 		List<PackResources> injected = new ArrayList<>(packs);
-		injected.add(new KubeJSClientResourcePack());
+		//KubeJS injected resources should have lower priority then user resources packs, which means file pack
+		int pos = injected.size();
+		for (int i = 0; i < injected.size(); i++) {
+			if (injected.get(i) instanceof FilePackResources) {
+				pos = i;
+				break;
+			}
+		}
+		injected.add(pos, new KubeJSClientResourcePack());
 		return injected;
 	}
 
