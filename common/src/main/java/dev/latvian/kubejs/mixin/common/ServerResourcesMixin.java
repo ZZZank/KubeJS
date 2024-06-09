fe412baa55e48e40077d01832d8aca01a397ffba
ZZZank <3410764033@qq.com>
2024-06-09 14:29:31 +0800

    remove(merge) DataPackRegistriesMixin

--- common/src/main/java/dev/latvian/kubejs/mixin/common/ServerResourcesMixin.java
+++ common/src/main/java/dev/latvian/kubejs/mixin/common/ServerResourcesMixin.java
@@ -3,10 +3,14 @@ package dev.latvian.kubejs.mixin.common;
 import dev.latvian.kubejs.server.ServerScriptManager;
 import net.minecraft.commands.Commands;
 import net.minecraft.server.ServerResources;
+import net.minecraft.server.packs.PackResources;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
 
 /**
  * @author LatvianModder
@@ -17,5 +21,12 @@ public abstract class ServerResourcesMixin {
 	private void init(Commands.CommandSelection commandSelection, int i, CallbackInfo ci) {
 		ServerScriptManager.instance = new ServerScriptManager();
 		ServerScriptManager.instance.init((ServerResources) (Object) this);
+	}
+
+	@ModifyArg(method = "loadResources", at = @At(value = "INVOKE", ordinal = 0,
+			target = "Lnet/minecraft/server/packs/resources/ReloadableResourceManager;reload(Ljava/util/concurrent/Executor;Ljava/util/concurrent/Executor;Ljava/util/List;Ljava/util/concurrent/CompletableFuture;)Ljava/util/concurrent/CompletableFuture;"),
+			index = 2)
+	private static List<PackResources> resourcePackList(List<PackResources> list) {
+		return ServerScriptManager.instance.resourcePackList(list);
 	}
 }
