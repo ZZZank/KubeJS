3d87df140bcc9922ad7aba7fca00a59f4c5efc5e
ZZZank <3410764033@qq.com>
2024-06-11 15:04:15 +0800

    injection point

--- common/src/main/java/dev/latvian/kubejs/mixin/common/ServerResourcesMixin.java
+++ common/src/main/java/dev/latvian/kubejs/mixin/common/ServerResourcesMixin.java
@@ -1,15 +1,12 @@
 package dev.latvian.kubejs.mixin.common;
 
-import dev.latvian.kubejs.KubeJS;
 import dev.latvian.kubejs.server.ServerScriptManager;
-import net.minecraft.commands.Commands;
 import net.minecraft.server.ServerResources;
 import net.minecraft.server.packs.PackResources;
+import net.minecraft.server.packs.PackType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 
@@ -18,17 +15,18 @@ import java.util.List;
  */
 @Mixin(ServerResources.class)
 public abstract class ServerResourcesMixin implements AutoCloseable {
-	@Inject(
+	@ModifyArg(
 			method = "<init>",
 			at = @At(
-					value = "NEW",
-					target = "net/minecraft/server/packs/resources/SimpleReloadableResourceManager"))
-	private void init(Commands.CommandSelection commandSelection, int i, CallbackInfo ci) {
-		//TODO: exactly the problem, command registry event is posted when `new Commands(...)` is called
-		//so we need to make injection point targeting somewhere before `this.commands = new Commands(arg);` in `ServerResources.<init>`
-		KubeJS.LOGGER.info("time stamping: ServerResources init");
+					value = "INVOKE",
+					target = "Lnet/minecraft/server/packs/resources/SimpleReloadableResourceManager;<init>(Lnet/minecraft/server/packs/PackType;)V")
+	)
+	private PackType init(PackType arg) {
+		//bad injection practice, but we need to make injection point targeting somewhere before `this.commands = new Commands(arg);` in `ServerResources.<init>`
 		ServerScriptManager.instance = new ServerScriptManager();
 		ServerScriptManager.instance.init((ServerResources) (Object) this);
+		ServerScriptManager.instance.loadFromDirectory();
+		return arg;
 	}
 
 	@ModifyArg(method = "loadResources", at = @At(value = "INVOKE", ordinal = 0,
