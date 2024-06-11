6fdf06cc6d62583a4730ae34054f1fc1deebfa8a
ZZZank <3410764033@qq.com>
2024-06-11 16:27:14 +0800

    tweak injection point again to prevent loading scripts twice
    finally found a good place to settle

--- common/src/main/java/dev/latvian/kubejs/mixin/common/ServerResourcesMixin.java
+++ common/src/main/java/dev/latvian/kubejs/mixin/common/ServerResourcesMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.server.packs.PackType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import java.util.List;
 
@@ -22,17 +23,14 @@ public abstract class ServerResourcesMixin implements AutoCloseable {
 					target = "Lnet/minecraft/server/packs/resources/SimpleReloadableResourceManager;<init>(Lnet/minecraft/server/packs/PackType;)V")
 	)
 	private PackType init(PackType arg) {
-		//bad injection practice, but we need to make injection point targeting somewhere before `this.commands = new Commands(arg);` in `ServerResources.<init>`
-		ServerScriptManager.instance = new ServerScriptManager();
+		//bad injection practice, but we need to make injection point targeting somewhere not "<init>"
 		ServerScriptManager.instance.init((ServerResources) (Object) this);
-		ServerScriptManager.instance.loadFromDirectory();
 		return arg;
 	}
 
-	@ModifyArg(method = "loadResources", at = @At(value = "INVOKE", ordinal = 0,
-			target = "Lnet/minecraft/server/packs/resources/ReloadableResourceManager;reload(Ljava/util/concurrent/Executor;Ljava/util/concurrent/Executor;Ljava/util/List;Ljava/util/concurrent/CompletableFuture;)Ljava/util/concurrent/CompletableFuture;"),
-			index = 2)
+	@ModifyVariable(method = "loadResources", at = @At(value = "HEAD", ordinal = 0), argsOnly = true)
 	private static List<PackResources> resourcePackList(List<PackResources> list) {
+		ServerScriptManager.instance = new ServerScriptManager();
 		return ServerScriptManager.instance.resourcePackList(list);
 	}
 }
